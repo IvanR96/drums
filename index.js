605645237a99bf32d70aff19f5e98a2338a1d80d
IvanR96 <irios96@hotmail.com>
2024-06-26 23:23:50 -0700

    sound test js good

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 for(let i = 0; i < document.querySelectorAll('.drum').length; i++){
    document.querySelectorAll('.drum')[i].addEventListener('click', function (){
-    
+      let audio = new Audio('tom-1.mp3');
+      audio.play();
 
    }); 
 }
