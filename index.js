79cc9d865e168742c2a9d75ed9add64d81998128
IvanR96 <irios96@hotmail.com>
2024-06-27 23:24:55 -0700

    add each sound to button

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 for(let i = 0; i < document.querySelectorAll('.drum').length; i++){
    document.querySelectorAll('.drum')[i].addEventListener('click', function (){
-      let audio = new Audio('tom-1.mp3');
-      audio.play();
+      
+      
 
    }); 
 }
 
+      //let audio = new Audio('tom-1.mp3');
+      //audio.play();
