c19211e4c8e7ddccd55690bba571570ba85c7542
IvanR96 <irios96@hotmail.com>
2024-06-29 22:13:23 -0700

    keydown event created

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
 for(let i = 0; i < document.querySelectorAll('.drum').length; i++){
    document.querySelectorAll('.drum')[i].addEventListener('click', function (){
       let buttonInnerHTML = this.innerHTML;
+
+      makeSound(buttonInnerHTML);
       
-      switch(buttonInnerHTML){
-         case "w":
-            let audio = new Audio('tom-1.mp3');
-            audio.play();
-            break;
-         case "a":
-            let audio1 = new Audio('tom-2.mp3');
-            audio1.play();
-            break;
-         case "s":
-            let audio2 = new Audio('tom-3.mp3');
-            audio2.play();
-            break;
-         case "d":
-            let audio3 = new Audio('tom-4.mp3');
-            audio3.play();
-            break;
-         case "j":
-            let audio4 = new Audio('crash.mp3');
-            audio4.play();
-            break;
-         case "k":
-            let audio5 = new Audio('snare.mp3');
-            audio5.play();
-            break;
-         case "l":
-            let audio6 = new Audio('kick-bass.mp3');
-            audio6.play();
-            break;
-         default:
-            break;
-
-            
-
-      }
+      
       
 
    }); 
 }
+
+
+function makeSound(key){
+   switch(key){
+      case "w":
+         let audio = new Audio('tom-1.mp3');
+         audio.play();
+         break;
+      case "a":
+         let audio1 = new Audio('tom-2.mp3');
+         audio1.play();
+         break;
+      case "s":
+         let audio2 = new Audio('tom-3.mp3');
+         audio2.play();
+         break;
+      case "d":
+         let audio3 = new Audio('tom-4.mp3');
+         audio3.play();
+         break;
+      case "j":
+         let audio4 = new Audio('crash.mp3');
+         audio4.play();
+         break;
+      case "k":
+         let audio5 = new Audio('snare.mp3');
+         audio5.play();
+         break;
+      case "l":
+         let audio6 = new Audio('kick-bass.mp3');
+         audio6.play();
+         break;
+      default:
+         break;
+
+         
+
+   }
+}
+
+//detect keyboard press
+document.addEventListener("keydown", function(e){
+   makeSound(e.key);
+})
 
 // factory example
 /*function HouseKeeper(yearsOfXp, name, cleaningArea){
